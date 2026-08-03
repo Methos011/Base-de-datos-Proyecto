7afb3c0bf58adfbad91e457de1e1d171dcdd722d
NicolasDier23 <nicodier@espol.edu.ec>
2026-08-02 19:32:42 -0500

    Update responsable.sql

--- SQL/CRUD/responsable.sql
+++ SQL/CRUD/responsable.sql
@@ -1,1 +1,37 @@
 
+-- create e insert
+
+CREATE TABLE responsable(
+    cedula int not null,
+    nombre varchar(50) not null,
+    apellido varchar(50) not null,
+    PRIMARY KEY (cedula)
+);
+
+INSERT INTO responsable (cedula, nombre, apellido)
+VALUES (1912345678, 'Carlos', 'Mendoza');
+
+INSERT INTO responsable (cedula, nombre, apellido)
+VALUES (1987654321, 'Ana', 'Torres');
+
+
+-- READ/consultas
+SELECT * FROM responsable;
+
+SELECT * FROM responsable
+WHERE apellido = 'Mendoza';
+
+
+-- update
+UPDATE responsable
+SET apellido = 'Mendoza Vera'
+WHERE cedula = 1912345678;
+
+UPDATE responsable
+SET nombre = 'Ana Maria'
+WHERE cedula = 1987654321;
+
+
+-- delete/ borrar
+DELETE FROM responsable
+WHERE cedula = 1987654321;
