d1113b51ca4930c9a6b8fff08cd1aa54485c97d9
Espinoza Sebas <sebqs05@gmail.com>
2026-08-08 09:09:14 -0500

    Update venta.py

--- Proyecto SBD/Ventas/venta.py
+++ Proyecto SBD/Ventas/venta.py
@@ -10,6 +10,8 @@ def menuVenta():
             print("0. Salir del menu de clientes")
             print("1. Mostrar ventas realizadas")
             print("2. Registrar venta")
+            print("3. Actualizar estado de venta")
+            print("4. Cancelar venta ya realizada")
             
             opcion = int(input("Seleccione una opcion del menu: "))
 
@@ -17,9 +19,13 @@ def menuVenta():
                 mostrar_ventas()
             elif opcion == 2:
                 registrar_venta()
-            
+            elif opcion == 3:
+                actualizar_venta()
+            elif opcion ==4:
+                eliminar_venta()
             elif opcion == 0:
                 print("Regresando al menu principal")
                 break;
             else:
                 print("Opcion incorrecta")
+            
