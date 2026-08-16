f320c3bec0ed9f14b7031ff8954c9ff01ba9643f
luis0908070605 <danpk0987654321@gmail.com>
2026-08-16 15:54:51 -0500

    Update Indices.sql

--- SQL/Indices.sql
+++ SQL/Indices.sql
@@ -1,1 +1,10 @@
+ 
+create index nombrecompleto on cliente(nombre,apellido);
 
+create index ind_telefono on cliente(telefono);
+
+create index ind_fechaventa on venta(fecha);
+
+create index ind_ventacliente on venta(cedula);
+
+create index ind_marcatipo on producto(marca,tipoproducto);
