6dff6314eb32a8d953799091dfba0d429be9dabf
Javier-Chavez-Anchundia <javichavanchu@gmail.com>
2026-08-16 16:56:49 -0500

    Agregar usuarios de la base de datos

--- SQL/Usuario.sql
+++ SQL/Usuario.sql
@@ -1,1 +1,19 @@
+-- CREACIÓN DE USUARIOS
 
+CREATE USER 'admin_deposito'@'localhost'
+IDENTIFIED BY 'Admin123';
+
+CREATE USER 'empleado_ventas'@'localhost'
+IDENTIFIED BY 'Ventas123';
+
+CREATE USER 'empleado_productos'@'localhost'
+IDENTIFIED BY 'Productos123';
+
+CREATE USER 'empleado_proveedores'@'localhost'
+IDENTIFIED BY 'Proveedores123';
+
+CREATE USER 'consultor'@'localhost'
+IDENTIFIED BY 'Consultor123';
+
+-- PERMISOS
+-- por hacer 
